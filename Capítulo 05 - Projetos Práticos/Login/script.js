edb0990f087291cd7ca3ca16f5ec213c2d90f859
Eder Taveira <edertaveira@gmail.com>
2023-02-11 14:20:32 -0300

    Colocando validação de formulário.

--- Capítulo 05 - Projetos Práticos/Login/script.js
+++ Capítulo 05 - Projetos Práticos/Login/script.js
@@ -1,12 +1,19 @@
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 const submit = document.getElementById("submit");
+const error = document.getElementById("error");
 
 submit.addEventListener("click", function(event) {
   event.preventDefault();
+  error.innerHTML = "";
+  if (!username.value || !password.value) {
+    error.innerHTML = "Todos os campos são obrigatórios";
+    return;
+  }
+
   if (username.value === "admin" && password.value === "123456") {
     alert("Login realizado com sucesso!");
   } else {
-    alert("Usuário ou senha inválidos.");
+    error.innerHTML = "Usuário ou senha inválidos.";
   }
 });
